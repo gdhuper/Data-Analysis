f9eda6778e8658231cc2f1b56add7ef6cdc2c44e
Gurpreet Singh <gdhuper@gmail.com>
2017-03-26 14:57:23 -0700

    cal mean std dev, min and max

--- Test/Test.py
+++ Test/Test.py
@@ -1,5 +1,8 @@
 import unicodecsv
 from datetime import datetime as dt
+from collections import defaultdict
+import numpy as np
+
 
 ## Longer version of code (replaced with shorter, equivalent version below)
 
@@ -164,10 +167,33 @@ for engagement_record in paid_engagement:
         paid_engagement_in_first_week.append(engagement_record)
 
 
-print("paid engagement in first week :"+str(len(paid_engagement_in_first_week)))
+print("paid engagement in first week :" + str(len(paid_engagement_in_first_week)))
+
+# Create a dictionary of engagement grouped by student.
+# The keys are account keys, and the values are lists of engagement records.
+engagement_by_account = defaultdict(list)
+for engagement_record in paid_engagement_in_first_week:
+    account_key = engagement_record['account_key']
+    engagement_by_account[account_key].append(engagement_record)
 
 
 
+# Create a dictionary with the total minutes each student spent in the classroom during the first week.
+# The keys are account keys, and the values are numbers (total minutes)
+total_minutes_by_account = {}
+for account_key, engagement_for_student in engagement_by_account.items():
+    total_minutes = 0
+    for engagement_record in engagement_for_student:
+        total_minutes += engagement_record['total_minutes_visited']
+    total_minutes_by_account[account_key] = total_minutes
+
+
+# Summarize the data about minutes spent in the classroom
+total_minutes = list(total_minutes_by_account.values())
+print('Mean:', np.mean(total_minutes))
+print('Standard deviation:', np.std(total_minutes))
+print('Minimum:', np.min(total_minutes))
+print('Maximum:', np.max(total_minutes))
 
 
 #printing stats
