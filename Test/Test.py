09532faf51e7548cd66c07a99b911bac8cf6ceb2
Gurpreet Singh <gdhuper@gmail.com>
2017-03-26 13:49:53 -0700

    getting paid engagements in first week

--- Test/Test.py
+++ Test/Test.py
@@ -123,9 +123,49 @@ for enrollment in non_udacity_enrollments:
     if not enrollment['is_canceled'] or enrollment['days_to_cancel'] > 7:
         account_key = enrollment['account_key']
         enrollment_date = enrollment['join_date']
-        paid_students[account_key] = enrollment_date
-        
+        if account_key not in paid_students or enrollment_date > paid_students[account_key]:
+            paid_students[account_key] = enrollment_date
+
 print("paid students: " + str(len(paid_students)))
+
+# Takes a student's join date and the date of a specific engagement record,
+# and returns True if that engagement record happened within one week
+# of the student joining.
+def within_one_week(join_date, engagement_date):
+    time_delta = engagement_date - join_date
+    return time_delta.days < 7
+
+def remove_free_trial_cancels(data):
+    new_data = []
+    for data_point in data:
+        if data_point['account_key'] in paid_students:
+            new_data.append(data_point)
+
+    return new_data
+
+paid_enrollments = remove_free_trial_cancels(non_udacity_enrollments)
+paid_engagement = remove_free_trial_cancels(non_udacity_engagement)
+paid_submissions = remove_free_trial_cancels(non_udacity_submissions)
+
+print(len(paid_enrollments))
+print(len(paid_engagement))
+print(len(paid_submissions))
+
+
+# calculating paid students for engagement
+paid_engagement_in_first_week = []
+
+for engagement_record in paid_engagement:
+    account_key = engagement_record['account_key']
+    join_date = paid_students[account_key]
+    engagement_record_date = engagement_record['utc_date']
+
+    if within_one_week(join_date, engagement_record_date):
+        paid_engagement_in_first_week.append(engagement_record)
+
+
+print("paid engagement in first week :"+str(len(paid_engagement_in_first_week)))
+
 
 
 
