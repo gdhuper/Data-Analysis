52841f063d0e4729f04dedd31ae8360ce2662138
Gurpreet Singh <gdhuper@gmail.com>
2017-04-04 22:56:08 -0700

    cleaning up

--- Test/Test.py
+++ Test/Test.py
@@ -17,7 +17,7 @@ def read_csv(filename):
     with open(filename, 'rb') as f:
         reader = unicodecsv.DictReader(f)
         return list(reader)
-
+// reading csv files into dictionaries
 enrollments = read_csv("enrollments.csv")
 daily_engagement = read_csv("daily_engagement.csv")
 project_submissions = read_csv("project_submissions.csv")
