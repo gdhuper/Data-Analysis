958fe08d86a26620d939b70fa25eb4aaed1b57bf
Gurpreet Singh <gdhuper@gmail.com>
2017-05-27 01:04:08 -0700

    Fixed read_csv

--- Test/Test.py
+++ Test/Test.py
@@ -13,7 +13,6 @@ import seaborn as sns
 # for row in reader:
 #     enrollments.append(row)
 # f.close()
-
 def read_csv(filename):
     with open(filename, 'rb') as f:
         reader = unicodecsv.DictReader(f)
